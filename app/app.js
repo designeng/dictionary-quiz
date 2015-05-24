38448dc4c789021ee0f086f5c037e827519d9e5a
Denis Savenok <denissavenok@gmail.com>
2015-05-24 13:04:19 +0400

    users.js removed

--- app/app.js
+++ app/app.js
@@ -2,13 +2,11 @@ var express     = require('express'),
     app         = express(),
     bodyParser  = require('body-parser'),
 
-    routes      = require('./routes/index'),
-    users       = require('./routes/users');
+    routes      = require('./routes/index');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api', routes);
-// app.use('/api/users', users);
 
 module.exports = app;
