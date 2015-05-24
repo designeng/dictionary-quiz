337356258700f2c2af2759102eefb66e5cd20615
Denis Savenok <denissavenok@gmail.com>
2015-05-24 12:17:45 +0400

    users rest methods: post, get, delete

--- app/app.js
+++ app/app.js
@@ -9,6 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api', routes);
-app.use('/api/users', users);
+// app.use('/api/users', users);
 
 module.exports = app;
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,44 @@ var express = require('express');
 var router  = express.Router();
 
 router.get('/', function(req, res) {
-  models.User.findAll({
-    include: [ models.Quiz ]
-  }).then(function(users) {
-    res.json({ users: users });
-  });
+    models.User.findAll({
+        include: [ models.Quiz ]
+    }).then(function(users) {
+        res.json({ users: users });
+    });
 });
+
+// Just to demonstrate REST methods.
+// in our quiz application user is a current player, while we have no registration. So, we have no update method.
+router.route("/users")
+    .post(function(req, res) {
+        var username = req.param('username')
+        models.User.create({
+            username: username
+        }).then(function() {
+            res.json({ message: 'User with username ' + username + ' created!' });
+        });
+    })  
+    .get(function(req, res) {
+        models.User.findAll({
+            include: [ models.Quiz ]
+        }).then(function(users) {
+            res.json({ users: users });
+        });
+    })
+    .delete(function(req, res) {
+        var user_id = req.param('user_id');
+        models.User.find({
+            where: {id: user_id},
+            include: [models.Quiz]
+        })
+        .then(function(user) {
+            models.Quiz.destroy({where: {UserId: user.id}}).then(function(affectedRows) {
+                user.destroy().then(function() {
+                    res.json({ message: 'User with id ' + user_id + ' deleted!' });
+                });
+            });
+        });
+    });
 
 module.exports = router;
