584524a17dd9d44e793ea225e577e6e22d5037f5
Denis Savenok <denissavenok@gmail.com>
2015-05-24 12:27:08 +0400

    username validation added

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,13 +14,17 @@ router.get('/', function(req, res) {
 // in our quiz application user is a current player, while we have no registration. So, we have no update method.
 router.route("/users")
     .post(function(req, res) {
-        var username = req.param('username')
-        models.User.create({
-            username: username
-        }).then(function() {
-            res.json({ message: 'User with username ' + username + ' created!' });
-        });
-    })  
+        var username = req.param('username').trim();
+        if(typeof username !== "undefined" && username.length){
+            models.User.create({
+                username: username
+            }).then(function() {
+                res.json({ message: 'User with username ' + username + ' created!' });
+            });
+        } else {
+            res.json({ error: 'User username should be defined!' });
+        }
+    })
     .get(function(req, res) {
         models.User.findAll({
             include: [ models.Quiz ]
